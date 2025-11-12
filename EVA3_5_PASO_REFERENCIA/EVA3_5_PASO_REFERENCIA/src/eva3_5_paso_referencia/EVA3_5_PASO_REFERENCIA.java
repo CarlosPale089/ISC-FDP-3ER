0dcea0dfee8f139135466560c8a8195184034176
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-11-11 23:56:02 -0600

    Add files via upload

---
+++ EVA3_5_PASO_REFERENCIA/EVA3_5_PASO_REFERENCIA/src/eva3_5_paso_referencia/EVA3_5_PASO_REFERENCIA.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_5_paso_referencia;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA3_5_PASO_REFERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int valor [] = new int [1];
+        valor[0] = 3;
+        System.out.println(valor);
+         System.out.println("valor (antes) = " + valor[0]);
+         incrementar(valor);
+         System.out.println("valor (despues) = " + valor[0]);
+    }
+    public static void incrementar(int[] val){
+        System.out.println(val);
+        for (int i = 0; i < val.length; i++) {
+            val[i] = val[i] + 1;
+            
+        }
+    }
+}
