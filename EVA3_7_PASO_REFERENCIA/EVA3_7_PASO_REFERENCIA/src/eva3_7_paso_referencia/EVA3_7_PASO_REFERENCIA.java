51a9fea8b26fd39911147d5f7bff670dbe224ee8
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-11-11 23:56:56 -0600

    Add files via upload

---
+++ EVA3_7_PASO_REFERENCIA/EVA3_7_PASO_REFERENCIA/src/eva3_7_paso_referencia/EVA3_7_PASO_REFERENCIA.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_7_paso_referencia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA3_7_PASO_REFERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String cliente[] = new String[5];
+        double credito[] = new double[5];
+        capturarClientes(cliente, credito);
+        imprimir(cliente, credito);
+        
+        
+    }
+     public static void capturarClientes(String[] nombre, double[] credito){
+         Scanner input = new Scanner (System.in);
+         for (int i = 0; i < credito.length; i++) {
+             System.out.println("Nombre del cliente: ");
+             nombre[i] = input.nextLine();
+             System.out.println("Credito del cliente: ");
+             credito[i] = input.nextInt();
+             input.nextLine();
+                     
+             
+         }
+     }
+      public static void imprimir(String[] nombre, double[] credito){
+           for (int i = 0; i < credito.length; i++) {
+               System.out.println("Nombre: " + nombre[i] + ", credito: $" + credito[i]);
+           }
+      }
+       public static void imprimir(String[] nombre){
+           for (int i = 0; i < nombre.length; i++) {
+               System.out.println("Nombre: " + nombre[i]);
+           }
+       }
+           }
